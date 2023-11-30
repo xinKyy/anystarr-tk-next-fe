ee8c78df32eb818712c876ab30b42e6c1bbdf1dd
xink <1611054604@qq.com>
2023-11-30 17:13:16 +0800

    fix:first commit

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,24 +6,12 @@ function Layout({ children }) {
     <>
       <style jsx>{`
         .content-container {
-          position: relative;
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-          margin: 70px 20px 20px 20px;
-          padding: 10px 20px;
-          background-color: #fff;
+           margin-top: 60px;
         }
       `}</style>
       <Header />
       <div className='content-container'>
         {children}
-      </div>
-      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-        Change Theme:
-        <DynamicAntdTheme
-          style={{ display: 'flex', marginLeft: '10px' }}
-        />
       </div>
     </>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,8 @@
-import Home from '../components/Home';
+
+import styles from '../components/Home/home.module.scss';
+
+const Home = ( ) =>{
+  return <div className={styles.home_page}>hhhh</div>;
+};
 
 export default Home;
