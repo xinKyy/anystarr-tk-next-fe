e1a515c1f833a5f377efd502717179ba8e45eb82
xink <1611054604@qq.com>
2023-12-01 22:32:33 +0800

    fix:首页

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,23 @@
 import styles from '../components/Home/home.module.scss';
 
 const Home = ( ) =>{
-  return <div className={styles.home_page}>hhhh</div>;
+  return <div className={styles.home_page}>
+    <div className={styles.home_bg}>
+      <div className={styles.home_radius}></div>
+      <div className={styles.home_front}></div>
+      <div className={styles.home_line}></div>
+    </div>
+    <div className={styles.home_qty_wrap}>
+      <div className={styles.home_start_all_wrap}>发行总量100,000,000</div>
+    </div>
+    <div className={styles.home_qty_wrap}>
+      <div className={`${styles.home_start_all_wrap} ${styles.coin_name_btn}`}>代币名称</div>
+    </div>
+    <div className={styles.home_qty_wrap}>
+      <div className={`${styles.home_start_all_wrap} ${styles.coin_name_btn}`}>代币名称</div>
+    </div>
+
+  </div>;
 };
 
 export default Home;
