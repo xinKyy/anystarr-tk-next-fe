c150d977cb270af8570119255522c31710ba20b2
xink <zyhh1611054604@gmail.com>
2024-10-18 23:13:53 +0800

    fix:Login modal

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -16,17 +16,35 @@ const SearchBar = ({onChange}) => {
   );
 
   return (
-    <div className={styles.searchBar}>
-      <Input
-        onChange={(e)=>{
-          debouncedOnChange(e.target.value);
-        }}
-        className={styles.inputField}
-        placeholder='Search product'
-      />
-      <div onClick={()=>debouncedOnChange(searchName.current)} className={styles.searchButton}>
-        <img src={"https://anystarr-image.oss-ap-southeast-1.aliyuncs.com/anystarr-next-asset/search.svg"} className={`${styles.icon} anticon`}>
-        </img>
+    <div style={{
+      display:"flex",
+      justifyContent:"space-between",
+      alignItems:"center",
+      width:"100%",
+      color:"#000"
+    }}>
+      <div style={{
+        color:"#000"
+      }}>
+        <a className={styles.a} href={"https://www.anystarr.com/en/terms-and-conditions/"}>
+          Terms and Conditions
+        </a>
+        <a className={styles.a} href={"https://www.anystarr.com/en/privacy-policy/"}>
+          Privacy Policy
+        </a>
+      </div>
+      <div className={styles.searchBar}>
+        <Input
+          onChange={(e)=>{
+            debouncedOnChange(e.target.value);
+          }}
+          className={styles.inputField}
+          placeholder='Search product'
+        />
+        <div onClick={()=>debouncedOnChange(searchName.current)} className={styles.searchButton}>
+          <img src={"https://anystarr-image.oss-ap-southeast-1.aliyuncs.com/anystarr-next-asset/search.svg"} className={`${styles.icon} anticon`}>
+          </img>
+        </div>
       </div>
     </div>
   );
