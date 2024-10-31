80e4adc19f2b49e6acc28f584088fda1aec01e6b
xink <zyhh1611054604@gmail.com>
2024-10-31 23:31:28 +0800

    fix:add

--- src/components/SoryBy/index.js
+++ src/components/SoryBy/index.js
@@ -5,7 +5,7 @@ const SortBy = ({current, onChange}) => {
     <div className={styles.container}>
 
       <div onClick={()=>onChange(5)} className={`${styles.sortItem} ${ current === 5 && styles.active}`}>
-        <div className={styles.itemText}>Random product</div>
+        <div className={styles.itemText}>Random Product</div>
         <div className={styles.arrows}>
           <div className={styles.arrowUp}></div>
           <div className={styles.arrowDown}></div>
