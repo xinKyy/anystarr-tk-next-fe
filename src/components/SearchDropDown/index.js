04cd3307d9569ded60349b787e77fdfd4d220815
xink <zyhh1611054604@gmail.com>
2024-11-29 01:02:46 +0800

    fix:首页card

--- src/components/SearchDropDown/index.js
+++ src/components/SearchDropDown/index.js
@@ -6,18 +6,25 @@ import {CloseCircleFilled} from "@ant-design/icons";
 const { Option } = Select;
 const SearchDropDown = ({zz}) => {
 
-  
+  const [history, setHistory] = useState([]);
+
+  if (history.length <= 0) return <div></div>;
+
   return (
     <div className={styles.SearchDropDown}>
       <div className={styles.dropDown}>
-          <div className={styles.dropDownItem}>
+          <div className={`${styles.dropDownItem} ${styles.no_hover}`}>
             <div></div>
             <div><img src='/static/historyTime.png' style={{withdth: 16, height: 16}}></img></div>
           </div>
-          <div className={styles.dropDownItem}>
-            <div>3123</div>
-            <div><CloseCircleFilled /></div>
-          </div>
+        {
+          history.map(item=>{
+            return <div className={styles.dropDownItem}>
+              <div>{item}</div>
+              <div><CloseCircleFilled /></div>
+            </div>;
+          })
+        }
       </div>
     </div>
   );
--- src/pages/product/[id].js
+++ src/pages/product/[id].js
@@ -22,153 +22,11 @@ function updateImageUrl(url, w, h) {
 
 
 const Product = ({productId}) =>{
-  const [product, setProduct] = useState();
-  const [mobile, setMobile] = useState(false);
-  const [collect, setCollect] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const [addTkLoading, setAddTkLoading] = useState(false);
-  const [showConnectTips, setShowConnectTips] = useState(false);
-  const { needLogin } = useLogin();
-  const onChange = (currentSlide) => {
-    console.log(currentSlide);
-  };
-
-  const getDetails = () =>{
-    APIGetProductInfo({productId:productId}).then(resp=>{
-      console.log(resp, "resp");
-      if (resp.data.product){
-        setProduct(resp.data.product);
-      }
-    });
-  };
-
-  useEffect(()=>{
-    setMobile(isMobile());
-    getDetails();
-  }, []);
-
-  const toAddTk = (e) =>{
-    e.stopPropagation();
-    window.gtag && window.gtag('event', 'add_to_showcase', {
-      'event_category': 'add_to_showcase',
-      'event_label': 'add_to_showcase',
-      'value': productId,
-    });
-    // if (needLogin){
-    //   setShowConnectTips(true);
-    //   return;
-    // }
-
-    if (addTkLoading) return;
-
-    if (product?.needApplyLink){
-      setAddTkLoading(true);
-      APIGetLinkByPid(product.productId).then(resp=>{
-        if (resp.data.url){
-          const url = resp.data.url;
-          if (isMobile()){
-            window.open(url, "_blank");
-          } else {
-            copy(url);
-            message.success("Copy link successfully, please open it with a browser on your mobile device");
-          }
-        }
-      }).finally(()=>{
-        setAddTkLoading(false);
-      });
-    } else {
-      const url = product?.url;
-      if (isMobile()){
-        window.open(url, "_blank");
-      } else {
-        copy(url);
-        message.success("Copy link successfully, please open it with a browser on your mobile device");
-      }
-    }
-
-  };
-
-  const addCollect = async () =>{
-    let user = localStorage.getItem("user");
-    if (user){
-      user = JSON.parse(user);
-      try {
-        const res = await APIAddFavoriteItems({
-          uid:user.id,
-          itemId:productId
-        });
-        if (res.data.result){
-          return ["Successfully added to favorites", null];
-        }
-        return [null, new Error(res.message)];
-      } catch (e){
-        return [null, new Error("Network Error")];
-      }
-    }
-    return [null, new Error("Please connect to Tiktok!")];
-  };
-
-  const removeCollect = async () =>{
-    let user = localStorage.getItem("user");
-    if (user){
-      user = JSON.parse(user);
-      try {
-        const res = await APIDeleteFavoriteItems({
-          uid:user.id,
-          itemId:productId
-        });
-        if (res.data.result){
-          return ["Unsubscribed successfully", null];
-        }
-        return [null, new Error(res.message)];
-      } catch (e){
-        return [null, new Error("Network Error")];
-      }
-    }
-    return [null, new Error("Please connect to Tiktok!")];
-  };
-
-  const opCollect = async () =>{
-    if (needLogin){
-      setShowConnectTips(true);
-      return;
-    }
-    if (loading){
-      return;
-    }
-    if (collect){
-      setLoading(true);
-      const [res, e] = await removeCollect();
-      setLoading(false);
-      if (e) return message.error(e.message);
-      message.success(res);
-      setCollect(false);
-      return ;
-    }
-    setLoading(true);
-    const [res, e] = await addCollect();
-    setLoading(false);
-    if (e) return message.error(e.message);
-    message.success(res);
-    setCollect(true);
-  };
-
   return <div>
-    {
-      showConnectTips && <ConnectTikTipsModal show={showConnectTips} onCancel={()=>setShowConnectTips(false)}></ConnectTikTipsModal>
-    }
-
-    {
-      !mobile && <SizeBox h={10}></SizeBox>
-    }
-
     <div className={styles.back_wrap}>
       <BackBtn></BackBtn>
     </div>
-    {
-      product ? <ProductDetails productId={productId}/> :
-        <div className={styles.product_wrap}><Skeleton /></div>
-    }
+    <ProductDetails productId={productId}/>
   </div>;
 
 };
@@ -312,86 +170,89 @@ const ProductDetails = ({productId}) =>{
     {
       showConnectTips && <ConnectTikTipsModal show={showConnectTips} onCancel={()=>setShowConnectTips(false)}></ConnectTikTipsModal>
     }
-    <div className={styles.product_detail_wrap}>
-      <div className={styles.left_wrapper}>
-        <Image
-          src={ product.alyImage ? product.alyImage : updateImageUrl(product?.image, 700, 700)}
-          className={styles.top_img}></Image>
-        {/* <div className={styles.swiper_wrap}>*/}
-        {/*  <div className={styles.swiper_item}></div>*/}
-        {/*  <div className={styles.swiper_item}></div>*/}
-        {/*  <div className={styles.swiper_item}></div>*/}
-        {/*  <div className={styles.swiper_item}></div>*/}
-        {/* </div>*/}
-      </div>
-      <div className={styles.gap}></div>
-      <div className={styles.right_wrapper}>
-        <div className={styles.title_wrap}>{product.title}</div>
-        <div className={styles.row_wrap}>
-          <div className={styles.row_item}>
-            <div className={styles.light_high}>{getEarn(product.price, Number(product.finishRate) / 100)}</div>
-            <div>Earn per sale</div>
-          </div>
-          <div className={styles.row_item}>
-            <div className={styles.normal_wrap}>{product.price}</div>
-            <div>Price</div>
-          </div>
-          <div className={styles.row_item}>
-            <div className={styles.normal_wrap}>{product.soldAmount}</div>
-            <div>Total sales</div>
-          </div>
-        </div>
-        <div className={styles.vs_wrap}>
-          <div className={styles.vs_left}>
-            <div>
-              <div></div>
-              <div className={styles.text_1}>Commission</div>
-              <div className={styles.high_light}>{product.finishRate}%</div>
-              <div className={styles.text_2}>Commission Rate</div>
-            </div>
-          </div>
-          <img className={styles.vs_icon} src={"https://anystarr-image.oss-ap-southeast-1.aliyuncs.com/anystarr-next-asset/vs_icon.png"} />
-          <div className={styles.vs_right}>
-            <div>
-              <div></div>
-              <div className={styles.text_1}>vs.open collab</div>
-              <div className={styles.gray_text}>{product.openRate}%</div>
-              <div className={styles.text_2}>Commission Rate</div>
-            </div>
-          </div>
-        </div>
-        <div className={styles.data_wrap}>
-          <div className={styles.item_wrap}>
-            <div className={styles.title_1}>Total sold</div>
-            <div>{product.soldNum}</div>
-          </div>
-          <div className={styles.item_wrap}>
-            <div className={styles.title_1}>Sold<span>(yesterday)</span></div>
-            <div>{product.daySoldNum}</div>
-          </div>
-          {/* <div className={styles.item_wrap}>*/}
-          {/*  <div className={styles.title_1}>Sold<span>(last 7 days)</span></div>*/}
-          {/*  <div>{product}</div>*/}
+
+    {
+      product ?     <div className={styles.product_detail_wrap}>
+        <div className={styles.left_wrapper}>
+          <Image
+            src={ product.alyImage ? product.alyImage : updateImageUrl(product?.image, 700, 700)}
+            className={styles.top_img}></Image>
+          {/* <div className={styles.swiper_wrap}>*/}
+          {/*  <div className={styles.swiper_item}></div>*/}
+          {/*  <div className={styles.swiper_item}></div>*/}
+          {/*  <div className={styles.swiper_item}></div>*/}
+          {/*  <div className={styles.swiper_item}></div>*/}
           {/* </div>*/}
         </div>
+        <div className={styles.gap}></div>
+        <div className={styles.right_wrapper}>
+          <div className={styles.title_wrap}>{product.title}</div>
+          <div className={styles.row_wrap}>
+            <div className={styles.row_item}>
+              <div className={styles.light_high}>{getEarn(product.price, Number(product.finishRate) / 100)}</div>
+              <div>Earn per sale</div>
+            </div>
+            <div className={styles.row_item}>
+              <div className={styles.normal_wrap}>{product.price}</div>
+              <div>Price</div>
+            </div>
+            <div className={styles.row_item}>
+              <div className={styles.normal_wrap}>{product.soldAmount}</div>
+              <div>Total sales</div>
+            </div>
+          </div>
+          <div className={styles.vs_wrap}>
+            <div className={styles.vs_left}>
+              <div>
+                <div></div>
+                <div className={styles.text_1}>Commission</div>
+                <div className={styles.high_light}>{product.finishRate}%</div>
+                <div className={styles.text_2}>Commission Rate</div>
+              </div>
+            </div>
+            <img className={styles.vs_icon} src={"https://anystarr-image.oss-ap-southeast-1.aliyuncs.com/anystarr-next-asset/vs_icon.png"} />
+            <div className={styles.vs_right}>
+              <div>
+                <div></div>
+                <div className={styles.text_1}>vs.open collab</div>
+                <div className={styles.gray_text}>{product.openRate}%</div>
+                <div className={styles.text_2}>Commission Rate</div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.data_wrap}>
+            <div className={styles.item_wrap}>
+              <div className={styles.title_1}>Total sold</div>
+              <div>{product.soldNum}</div>
+            </div>
+            <div className={styles.item_wrap}>
+              <div className={styles.title_1}>Sold<span>(yesterday)</span></div>
+              <div>{product.daySoldNum}</div>
+            </div>
+            {/* <div className={styles.item_wrap}>*/}
+            {/*  <div className={styles.title_1}>Sold<span>(last 7 days)</span></div>*/}
+            {/*  <div>{product}</div>*/}
+            {/* </div>*/}
+          </div>
 
-        <div className={styles.btn_wrap}>
-          {
-            product.collect ? <div onClick={opCollect} className={styles.remove_collection}>
-              Uncollections
-            </div> : <div onClick={opCollect} className={styles.add_to_collection}>Add to Collection</div>
-          }
-          <div className={styles.gap}></div>
-          <div onClick={toAddTk} className={styles.add_to_showcase}>
+          <div className={styles.btn_wrap}>
             {
-              addTkLoading && <LoadingOutlined></LoadingOutlined>
+              product.collect ? <div onClick={opCollect} className={styles.remove_collection}>
+                Uncollections
+              </div> : <div onClick={opCollect} className={styles.add_to_collection}>Add to Collection</div>
             }
-            Add to Showcase
+            <div className={styles.gap}></div>
+            <div onClick={toAddTk} className={styles.add_to_showcase}>
+              {
+                addTkLoading && <LoadingOutlined></LoadingOutlined>
+              }
+              Add to Showcase
+            </div>
           </div>
         </div>
-      </div>
-    </div>
-    {/* 模态框 */}
+      </div> : <div className={styles.product_wrap}><Skeleton /></div>
+    }
+
     <ProductModal />
   </>;
 };
